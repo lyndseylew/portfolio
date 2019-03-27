9614f849998c593f9a3382d9eaeab410e73fbf1a
Lyndsey <lyndsey.a.lewis@gmail.com>
2019-03-26 20:14:10 -0400

    Added navigation functionality and formatting

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let mainNav = document.getElementById('nav-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+navBarToggle.addEventListener('click', function () {
+    mainNav.classList.toggle('active');
+});
