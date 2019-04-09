e543c41beee6ccc0291b326dda1a3094e24ddcf7
Lyndsey <lyndsey.a.lewis@gmail.com>
2019-04-09 08:43:52 -0400

    Added draft Typed.js to hero area (this still needs work)

--- js/main.js
+++ js/main.js
@@ -3,3 +3,41 @@ let navBarToggle = document.getElementById('js-navbar-toggle');
 navBarToggle.addEventListener('click', function () {
     mainNav.classList.toggle('active');
 });
+
+
+$(function(){
+
+  $("#typed").typed({
+    strings: ["finding creative solutions", "clean code", "coffee", "beautiful and useful websites", "plants"],
+    typeSpeed: 40,
+    backSpeed: 40,
+    backDelay: 1000,
+    loop: true,
+    callback: function(){
+      shift();
+    }
+  });
+
+});
+
+function shift(){
+    $(".head-wrap").addClass("shift-text");
+    terminalHeight();
+}
+
+function terminalHeight(){
+    var termHeight = $(".terminal-height");
+    var value = termHeight.text();
+    value = parseInt(value);
+    setTimeout(function(){
+        if (value > 10){
+            value = value-1;
+            this.txtValue = value.toString();
+            termHeight.text(this.txtValue);
+            self.terminalHeight();
+        }
+        else{
+            clearTimeout();
+        }
+    }, 10);
+}
